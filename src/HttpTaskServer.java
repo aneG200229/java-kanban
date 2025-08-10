d1eab63011e7c498a189f19b4368a8f74c017d92
Gena Lobanov <gena.lobanov@yandex.ru>
2025-08-10 19:25:47 +0300

    Исправление ошибок фз 9

--- src/HttpTaskServer.java
+++ src/HttpTaskServer.java
@@ -1,23 +1,19 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.InetSocketAddress;
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.List;
+
 
 public class HttpTaskServer {
     private HttpServer server;
     private Gson gson;
-    private TaskManager manager;
+
 
     public HttpTaskServer(TaskManager manager) throws IOException {
-        this.manager = manager;
         this.gson = new GsonBuilder().registerTypeAdapter(LocalDateTime.class, new BaseHttpHandler.LocalDateTimeAdapter())
                 .registerTypeAdapter(Duration.class, new BaseHttpHandler.DurationAdapter()).create();
         this.server = HttpServer.create(new InetSocketAddress(8080), 0);
